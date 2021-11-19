eaab38fc048e515459a981b5e6dfd2e81695b6ec
TheCatWest <catwestover@outlook.com>
2021-11-19 15:50:51 -0700

    set up header and button components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+// component declaration
 @Component({
+// selector is whatever the html tag is that you'll use to embed component
+// bc this is the root component, is being embedded in app-root in html file
   selector: 'app-root',
+  // html file that you're using
   templateUrl: './app.component.html',
+  // your style/css file; can have more than one
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task-app-ng';
+  // class is where you add any properties and methods of the component
+  // it's suggested to add types to properties and functions
+  // moved the title from app-components.ts and added to header-component.ts
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
+// module brought in from Angular/core
+// using CLI to make compunents does everything automatically
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
